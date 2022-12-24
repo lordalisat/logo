4f2e9b3afd1ebcdadfb28e4dbde7ab184139049a
Bram Wieringa <b.wieringa31@gmail.com>
2022-12-24 21:19:12 +0100

    Init T3 App

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
