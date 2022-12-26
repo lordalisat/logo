305c0447568711882cc060e29e2e1489525f6183
Bram Wieringa <b.wieringa31@gmail.com>
2022-12-26 21:08:02 +0100

    Add simple login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,27 +4,17 @@ import GoogleProvider from "next-auth/providers/google";
 import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
+  theme: {
+    colorScheme: "auto", // "auto" | "dark" | "light"
+    brandColor: "#91ff00", // Hex color code
+    logo: "", // Absolute URL to image
+    buttonText: "#91ff00" // Hex color code
   },
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
     })
     // ...add more providers here
   ],
