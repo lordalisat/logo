e8cbc753cfe43db16da7d9f689f3d5fc24299d7f
Bram Wieringa <b.wieringa31@gmail.com>
2022-12-31 14:10:00 +0100

    Add new Color picker

--- src/components/color-picker.tsx
+++ src/components/color-picker.tsx
@@ -1,5 +1,14 @@
-export default function ColorPicker() {
-  return <>
-    
-  </>
+import React from 'react'
+import { HexColorInput, HexColorPicker } from "react-colorful";
+import type { ColorPickerBaseProps } from 'react-colorful/dist/types';
+
+const MyPicker = ({ color, onChange }: ColorPickerBaseProps<string>) => {
+  return (
+    <>
+      <HexColorInput color={color} onChange={onChange} />
+      <HexColorPicker color={color} onChange={onChange} />
+    </>
+  )
 }
+
+export default MyPicker;
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import ColorPicker from "./color-picker";
 import FooterButtons from "./footer-buttons";
 
 export default function MainContent() {
+  const [color, setColor] = useState("#FFFFFF")
+
   return <>
-    <div className="flex-grow overflow-y-auto">
-      <ColorPicker />
+    <div className="flex-grow overflow-y-auto overflow-x-hidden">
+      <ColorPicker
+        color={color}
+        onChange={ setColor } />
     </div>
     <FooterButtons />
   </>
