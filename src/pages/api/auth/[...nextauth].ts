652b7accb3e3f8974a677dfa84508208e9ebf549
Bram Wieringa <b.wieringa31@gmail.com>
2023-03-03 20:53:13 +0100

    Remove unused branding

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,12 +4,6 @@ import GoogleProvider from "next-auth/providers/google";
 import { env } from "env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-  theme: {
-    colorScheme: "auto", // "auto" | "dark" | "light"
-    brandColor: "#91ff00", // Hex color code
-    logo: "", // Absolute URL to image
-    buttonText: "#91ff00" // Hex color code
-  },
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
